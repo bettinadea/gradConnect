bc291f47228c936dc1f9b37315a579fd6c12d338
Andrew Sie <asie@ucsd.edu>
2015-02-05 22:32:02 -0800

    stuff

--- app.js
+++ app.js
@@ -12,7 +12,18 @@ var index = require('./routes/index');
 var clinicalcognitionchoice = require('./routes/clinicalcognitionchoice');
 var clinicalprofessors = require('./routes/clinicalprofessors');
 var clinicalgrads = require('./routes/clinicalgrads');
-
+var computationchoice = require('./routes/computationchoice');
+var computationgrads = require('./routes/computationgrads');
+var computationprofessors = require('./routes/computationprofessors');
+var hcichoice = require('./routes/hcichoice');
+var hcigrads = require('./routes/hcigrads');
+var hciprofessors = require('./routes/hciprofessors');
+var humancognitionchoice = require('./routes/humancognitionchoice');
+var humancognitiongrads = require('./routes/humancognitiongrads');
+var humancognitionprofessors = require('./routes/humancognitionprofessors');
+var neurosciencechoice = require('./routes/neurosciencechoice');
+var neurosciencegrads = require('./routes/neurosciencegrads');
+var neuroscienceprofessors = require('./routes/neuroscienceprofessors');
 
 // Example route
 // var user = require('./routes/user');
@@ -44,6 +55,18 @@ app.get('/', index.view);
 app.get('/clinicalcognitionchoice', clinicalcognitionchoice.view);
 app.get('/clinicalprofessors', clinicalprofessors.view);
 app.get('/clinicalgrads', clinicalgrads.view);
+app.get('/computationchoice', computationchoice.view);
+app.get('/computationprofessors', computationprofessors.view);
+app.get('/computationgrads', computationgrads.view);
+app.get('/hcichoice', hcichoice.view);
+app.get('/hciprofessors', hciprofessors.view);
+app.get('/hcigrads', hcigrads.view);
+app.get('/humancognitionchoice', humancognitionchoice.view);
+app.get('/humancognitionprofessors', humancognitionprofessors.view);
+app.get('/humancognitiongrads', humancognitiongrads.view);
+app.get('/neurosciencechoice', neurosciencechoice.view);
+app.get('/neuroscienceprofessors', neuroscienceprofessors.view);
+app.get('/neurosciencegrads', neurosciencegrads.view);
 // Example route
 // app.get('/users', user.list);
 
