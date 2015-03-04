e0962ca3763a1bccbd201cc788145c68dfc8c19b
Andrew Sie <asie@ucsd.edu>
2015-03-03 21:26:54 -0800

    connected woopra

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,12 @@ var data = require('../data.json');
 
 exports.view = function(req, res){
 	console.log(data);
-	res.render('index', data);
+	//res.render('index', data);
+	var random_num = Math.random();
+
+	if(random_num > 0.5){
+  	 	res.render("index", data);
+  	}else{
+    	res.render("index_alternate", data);
+  	}
 };
